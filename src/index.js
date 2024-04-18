cfe512a353cdbcf9390e01e1453579af2d7695e1
Rebeca Rodríguez <rebeca.rodriguez.guirao@gmail.com>
2024-04-19 01:12:10 +0200

    init server and DB config

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenev from 'dotenv';
+import { testConnection } from './db.js';
+import userRoutes from './routes/userRoutes.js';
+
+dotenev.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+await testConnection();
+
+// Configurar rutas
+app.use('/user', userRoutes);
+
+// Iniciar el servidor
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000');
+});
+
+app.get('/', (req, res) => {
+    res.send('Api running...');
+  });
+
+
+
+
